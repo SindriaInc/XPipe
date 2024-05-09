51c4ce016c3c2a75f30f726b4ac992c508e5d57e
lucapitzoi <luca.pitzoi@sindria.org>
2024-05-09 20:18:50 +0200

    update

--- Docs/handouts/bin/build.sh
+++ Docs/handouts/bin/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 CONTAINER_NAME=handouts
-FILEMD=cloud.md
-FILENAME=corso_cloud_modulo1_v1.pdf
+FILEMD=questions_foundation.md
+FILENAME=corso_fondamenti_di_informatica_modulo1_v1.pdf
 
 docker exec -it ${CONTAINER_NAME} pandoc ${FILEMD} -o out.pdf
 docker exec -it ${CONTAINER_NAME} mv /var/www/app/out.pdf /var/www/app/out/${FILENAME}
