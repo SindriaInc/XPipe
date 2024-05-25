fad73796f80fd956f73b60a67938a8b4bf8b4b4d
lucapitzoi <luca.pitzoi@sindria.org>
2024-05-26 01:04:36 +0200

    BaseImages: update prometheus

--- BaseImages/prometheus/src/resources/bin/entrypoint.sh
+++ BaseImages/prometheus/src/resources/bin/entrypoint.sh
@@ -1,14 +1,60 @@
 #!/usr/bin/env sh
 
+APEX="'"
+COMMA=","
+
 # Setting global env
-sed -i -E "s|@@DEFAULT_RECEIVER@@|${DEFAULT_RECEIVER}|g" /etc/alertmanager/config.yml
-sed -i -E "s|@@DASHBOARD_URL@@|${DASHBOARD_URL}|g" /etc/alertmanager/sindria.tmpl
+sed -i -E "s|@@MONITOR_LABEL@@|${MONITOR_LABEL}|g" /etc/prometheus/prometheus.yml
 
-# Setting telegram env
-sed -i -E "s|@@TELEGRAM_BOT_TOKEN@@|${TELEGRAM_BOT_TOKEN}|g" /etc/alertmanager/config.yml
-sed -i -E "s|@@TELEGRAM_CHAT_ID@@|${TELEGRAM_CHAT_ID}|g" /etc/alertmanager/config.yml
+# Setting alertmanager env
+sed -i -E "s|@@ALERTMANAGER_SCHEME@@|${ALERTMANAGER_SCHEME}|g" /etc/prometheus/prometheus.yml
+sed -i -E "s|@@ALERTMANAGER_HOST@@|${ALERTMANAGER_HOST}|g" /etc/prometheus/prometheus.yml
+sed -i -E "s|@@ALERTMANAGER_PORT@@|${ALERTMANAGER_PORT}|g" /etc/prometheus/prometheus.yml
 
-# Setting teams env
-sed -i -E "s|@@TEAMS_WEBHOOK_URL@@|${TEAMS_WEBHOOK_URL}|g" /etc/alertmanager/config.yml
+# Setting scrape config env
+sed -i -E "s|@@SCRAPE_CONFIG@@|${SCRAPE_CONFIG}|g" /etc/prometheus/prometheus.yml
 
-/bin/alertmanager --config.file=/etc/alertmanager/config.yml --storage.path=/alertmanager
+# Setting self toggle env
+if [ "${SELF_TOGGLE}" == "1" ]; then
+  sed -i -E "s|#@@||g" /etc/prometheus/prometheus.yml
+fi
+
+# Setting scrape config self env
+sed -i -E "s|@@SELF_HOST@@|${SELF_HOST}|g" /etc/prometheus/configs/self.yml
+sed -i -E "s|@@SELF_PORT@@|${SELF_PORT}|g" /etc/prometheus/configs/self.yml
+
+# Parse MONITORING_HOSTS env separated by ;
+
+TOTAL=""
+FORMATTED_MONITORING_HOSTS=""
+
+for i in $(echo $MONITORING_HOSTS | tr ";" "\n")
+do
+  ENTRY=$APEX$i$APEX
+  SINGLE=$ENTRY$COMMA
+  TOTAL=$TOTAL$SINGLE
+done
+
+FORMATTED_MONITORING_HOSTS=${TOTAL::len-1}
+
+# Setting scrape config monitoring env
+sed -i -E "s|@@MONITORING_HOSTS@@|${FORMATTED_MONITORING_HOSTS}|g" /etc/prometheus/configs/monitoring.yml
+
+# Parse APM_HOSTS env separated by ;
+
+TOTAL=""
+FORMATTED_APM_HOSTS=""
+
+for i in $(echo $APM_HOSTS | tr ";" "\n")
+do
+  ENTRY=$APEX$i$APEX
+  SINGLE=$ENTRY$COMMA
+  TOTAL=$TOTAL$SINGLE
+done
+
+FORMATTED_APM_HOSTS=${TOTAL::len-1}
+
+# Setting scrape config apm env
+sed -i -E "s|@@APM_HOSTS@@|${FORMATTED_APM_HOSTS}|g" /etc/prometheus/configs/apm.yml
+
+/bin/prometheus
