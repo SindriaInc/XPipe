5a33fdef644b59d966572f8bb3930b1335262d6f
lucapitzoi <luca.pitzoi@sindria.org>
2024-11-04 09:04:43 +0100

    update

---
+++ IaC/wopr/tls/update_secrets.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# export AWS_PROFILE=devops
+AWS_PROFILE=devops
+
+# Sync liviomunaridecorazioni.it
+aws s3 sync s3://sindria-lmd-certbot-cache/k8s/ .
+
+# Sync monsterhunter.it and monster-hunter.it
+aws s3 sync s3://sindria-mh-certbot-cache/k8s/ .
+
+# Update liviomunaridecorazioni.it
+kubectl apply -f liviomunaridecorazioni.it/tls-liviomunaridecorazioniit.yaml -n lmd-website
+
+# Update monsterhunter.it
+kubectl apply -f monsterhunter.it/tls-monsterhunterit.yaml -n mh-website
+# Update monster-hunter.it
+kubectl apply -f monster-hunter.it/tls-monster-hunterit.yaml -n mh-website
