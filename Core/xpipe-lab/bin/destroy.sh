a166a37f56d4a6a94ad0bef4df391da9a15d41fe
lucapitzoi <luca.pitzoi@sindria.org>
2024-11-28 01:38:16 +0100

    update

--- Core/xpipe-lab/bin/destroy.sh
+++ Core/xpipe-lab/bin/destroy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker stop xpipe-xdev-lab-control-plane
-docker stop xpipe-xdev-lab-worker
+docker stop xpipe-lab-control-plane
+docker stop xpipe-lab-worker
 
-docker rm xpipe-xdev-lab-control-plane
-docker rm xpipe-xdev-lab-worker
+docker rm xpipe-lab-control-plane
+docker rm xpipe-lab-worker
