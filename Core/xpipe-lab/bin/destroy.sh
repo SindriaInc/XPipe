d6fc4008270a264c157013c1ed1f6e43a8e724c0
lucapitzoi <luca.pitzoi@sindria.org>
2024-11-29 20:43:09 +0100

    Core: update xpipe-lab

--- Core/xpipe-lab/bin/destroy.sh
+++ Core/xpipe-lab/bin/destroy.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Setting Colors
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' #No Color
+NOW=$(date "+%Y-%m-%d_%H-%M-%S")
+
+echo -e "${BLUE}Stopping services...${NC}"
 docker stop xpipe-lab-control-plane
 docker stop xpipe-lab-worker
+echo
 
+echo -e "${BLUE}Removing services...${NC}"
 docker rm xpipe-lab-control-plane
 docker rm xpipe-lab-worker
+echo
+
+echo -e "${BLUE}Done.${NC}"
