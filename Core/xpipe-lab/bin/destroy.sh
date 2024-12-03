7ea1a18f0928bc931658abe2fcddae58911f936d
lucapitzoi <luca.pitzoi@sindria.org>
2024-12-03 21:47:51 +0100

    update

--- Core/xpipe-lab/bin/destroy.sh
+++ Core/xpipe-lab/bin/destroy.sh
@@ -10,13 +10,17 @@ NC='\033[0m' #No Color
 NOW=$(date "+%Y-%m-%d_%H-%M-%S")
 
 echo -e "${BLUE}Stopping services...${NC}"
-docker stop xpipe-lab-control-plane
-docker stop xpipe-lab-worker
+docker stop xpipe-lab-control-plane || true
+docker stop xpipe-lab-worker || true
 echo
 
 echo -e "${BLUE}Removing services...${NC}"
-docker rm xpipe-lab-control-plane
-docker rm xpipe-lab-worker
+docker rm xpipe-lab-control-plane || true
+docker rm xpipe-lab-worker || true
+echo
+
+echo -e "${BLUE}Removing init...${NC}"
+docker compose down -v
 echo
 
 echo -e "${BLUE}Done.${NC}"
