068a9ef1a9130b361952210d7333241f17394dc4
lucapitzoi <luca.pitzoi@sindria.org>
2024-12-24 16:18:33 +0100

    update

--- Lib/nanoREST/src/bin/build.sh
+++ Lib/nanoREST/src/bin/build.sh
@@ -7,5 +7,5 @@ docker exec -t ${CONTAINER_NAME} su sindria -c "mvn compile; mvn package"
 #docker exec -t ${CONTAINER_NAME} /bin/bash -c "java -jar target/${CONTAINER_NAME}-${APP_VERSION}.jar"
 
 echo "Deploy local"
-cp /Users/lucapitzoi/XPipe/Lib/nanoREST/src/target/nanoREST-${APP_VERSION}.jar /Users/lucapitzoi/XPipe/Lib/tca-scoreboards/src/libs/nanoREST-${APP_VERSION}.jar
+cp /Users/lucapitzoi/XPipe/Lib/nanoREST/src/target/nanoREST-${APP_VERSION}.jar /Users/lucapitzoi/XPipe/Lib/blog/src/libs/nanoREST-${APP_VERSION}.jar
 echo "Done"
