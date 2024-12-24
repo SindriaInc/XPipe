b93dd402ccf3c136743eac7f6a4786e1acefd814
lucapitzoi <luca.pitzoi@sindria.org>
2024-12-24 16:17:14 +0100

    Lib: bootstrap nanoREST and blog

--- Lib/blog/readme.md
+++ Lib/blog/readme.md
@@ -1,11 +1,9 @@
-# Repo Name
+# Blog
 
-This repo is for purpose repo.
+Blog example developed with nanoREST framework.
 
 ## Setup Development Environment
 
-- Clone this repo: `git clone git@git.sindria.org:<project-namespace>/<repo-name>.git`
-- Move into it: `cd <repo-name>`
 - Setup env: `cp .env.local .env`
 - Setup docker compose: `cp docker-compose.local.yml docker-compose.yml`
 - Start environment: `docker-compose up -d`
--- Lib/nanoREST/build.sh
+++ Lib/nanoREST/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z "$1" ]]; then
-    echo "Provide image name as first argument (eg. registry.sindria.org/<image-namespace>/<image-name>)"
+    echo "Provide image name as first argument (eg. sindriaproject/<repo-slug>)"
     exit 1
 fi
 
@@ -10,16 +10,23 @@ if [[ -z "$2" ]]; then
     exit 1
 fi
 
+if [[ -z "$3" ]]; then
+    echo "Provide a tag arch as fourth argument (eg. amd64, arm64)"
+    exit 1
+fi
+
 
 IMAGE_NAME=$1
 TAG_VERSION=$2
+TAG_ARCH=$3
 
 HOST_USER_UID=1000
 TIMEZONE=Europe/Rome
 
 docker build ./src \
-    --tag ${IMAGE_NAME}:${TAG_VERSION} \
+    --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_ARCH} \
     --tag ${IMAGE_NAME}:latest \
+    --build-arg ARCH=${TAG_ARCH} \
     --build-arg TAG_VERSION=${TAG_VERSION} \
     --build-arg HOST_USER_UID=${HOST_USER_UID} \
     --build-arg TIMEZONE=${TIMEZONE}
