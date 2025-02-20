60df55514498d66769ec193f5fa36e23fca33fc7
lucapitzoi <luca.pitzoi@sindria.org>
2025-02-20 14:19:21 +0100

    Lib: added JsonSerializer

--- Lib/nanoREST/src/bin/build.sh
+++ Lib/nanoREST/src/bin/build.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
+#set -e
+
+# Setting Colors
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' #No Color
+NOW=$(date "+%Y-%m-%d_%H-%M-%S")
+
 CONTAINER_NAME=nanoREST
-APP_VERSION=1.0.0
+NANOREST_VERSION=1.1.0
 
+echo -e "${BLUE}Building and Packaging...${NC}"
 docker exec -t ${CONTAINER_NAME} bash -c "mvn compile; mvn package"
-#docker exec -t ${CONTAINER_NAME} /bin/bash -c "java -jar target/${CONTAINER_NAME}-${APP_VERSION}.jar"
+echo
 
-echo "Deploy local"
-cp /Users/lucapitzoi/XPipe/Lib/nanoREST/src/target/nanoREST-${APP_VERSION}.jar /Users/lucapitzoi/XPipe/Lib/blog/src/libs/nanoREST-${APP_VERSION}.jar
-echo "Done"
+echo -e "${BLUE}Deploy local...${NC}"
+cp /Users/lucapitzoi/XPipe/Lib/nanoREST/src/target/nanoREST-${NANOREST_VERSION}.jar /Users/lucapitzoi/XPipe/Lib/blog/src/libs/nanoREST-${NANOREST_VERSION}.jar
+echo
+
+echo -e "${BLUE}Deploy artifact...${NC}"
+echo
+
+
+echo -e "${BLUE}Done.${NC}"
