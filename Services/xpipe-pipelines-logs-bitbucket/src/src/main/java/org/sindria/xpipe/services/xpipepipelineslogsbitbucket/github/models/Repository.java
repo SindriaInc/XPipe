50de2b6c6910d4cec4848b6959443dc161b24981
Alberto Biggiogero <alberto.biggiogero@sindria.org>
2025-02-27 11:24:38 +0100

    Services: add all optional feld to repo model

--- Services/xpipe-pipelines-logs-bitbucket/src/src/main/java/org/sindria/xpipe/services/xpipepipelineslogsbitbucket/github/models/Repository.java
+++ Services/xpipe-pipelines-logs-bitbucket/src/src/main/java/org/sindria/xpipe/services/xpipepipelineslogsbitbucket/github/models/Repository.java
@@ -3,9 +3,6 @@ package org.sindria.xpipe.services.xpipepipelineslogsbitbucket.github.models;
 import org.json.JSONObject;
 import org.sindria.xpipe.lib.nanoREST.serializers.JsonSerializer;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 public class Repository {
 
     private final String name;
@@ -22,6 +19,38 @@ public class Repository {
 
     private final boolean hasWiki;
 
+    private final boolean hasDownloads;
+
+    private final boolean isTemplate;
+
+    private final int teamId;
+
+    private final boolean autoInit;
+
+    private final String gitignoreTemplate;
+
+    private final String licenseTemplate;
+
+    private final boolean allowSquashMerge;
+
+    private final boolean allowMergeCommit;
+
+    private final boolean allowMergeRebase;
+
+    private final boolean allowAutoMerge;
+
+    private final boolean deleteBranchOnMerge;
+
+    private final boolean useSquashPrTitleAsDefault;
+
+    private final String squashMergeCommitTitle;
+
+    private final String squashMergeCommitMessage;
+
+    private final String mergeCommitTitle;
+
+    private final String mergeCommitMessage;
+
     // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#create-an-organization-repository
 
     public Repository(String name) {
@@ -32,9 +61,49 @@ public class Repository {
         this.hasIssues = true;
         this.hasProjects =  true;
         this.hasWiki = true;
+        this.hasDownloads = true;
+        this.isTemplate = false;
+        this.teamId = 0;
+        this.autoInit = false;
+        this.gitignoreTemplate = "";
+        this.licenseTemplate = "";
+        this.allowSquashMerge = true;
+        this.allowMergeCommit = true;
+        this.allowMergeRebase = true;
+        this.allowAutoMerge = false;
+        this.deleteBranchOnMerge = false;
+        this.useSquashPrTitleAsDefault = false;
+        this.squashMergeCommitTitle = "";
+        this.squashMergeCommitMessage = "";
+        this.mergeCommitTitle = "";
+        this.mergeCommitMessage = "";
     }
 
-    public Repository(String name, String description, String homepage, boolean isPrivate,  boolean hasIssues, boolean hasProjects, boolean hasWiki) {
+    public Repository(
+            String name,
+            String description,
+            String homepage,
+            boolean isPrivate,
+            boolean hasIssues,
+            boolean hasProjects,
+            boolean hasWiki,
+            boolean hasDownloads,
+            boolean isTemplate,
+            int teamId,
+            boolean autoInit,
+            String gitignoreTemplate,
+            String licenseTemplate,
+            boolean allowSquashMerge,
+            boolean allowMergeCommit,
+            boolean allowMergeRebase,
+            boolean allowAutoMerge,
+            boolean deleteBranchOnMerge,
+            boolean useSquashPrTitleAsDefault,
+            String squashMergeCommitTitle,
+            String squashMergeCommitMessage,
+            String mergeCommitTitle,
+            String mergeCommitMessage
+    ) {
         this.name = name;
         this.description = description;
         this.homepage = homepage;
@@ -42,6 +111,22 @@ public class Repository {
         this.hasIssues = hasIssues;
         this.hasProjects = hasProjects;
         this.hasWiki = hasWiki;
+        this.hasDownloads = hasDownloads;
+        this.isTemplate = isTemplate;
+        this.teamId = teamId;
+        this.autoInit = autoInit;
+        this.gitignoreTemplate = gitignoreTemplate;
+        this.licenseTemplate = licenseTemplate;
+        this.allowSquashMerge = allowSquashMerge;
+        this.allowMergeCommit = allowMergeCommit;
+        this.allowMergeRebase = allowMergeRebase;
+        this.allowAutoMerge = allowAutoMerge;
+        this.deleteBranchOnMerge = deleteBranchOnMerge;
+        this.useSquashPrTitleAsDefault = useSquashPrTitleAsDefault;
+        this.squashMergeCommitTitle = squashMergeCommitTitle;
+        this.squashMergeCommitMessage = squashMergeCommitMessage;
+        this.mergeCommitTitle = mergeCommitTitle;
+        this.mergeCommitMessage = mergeCommitMessage;
     }
 
 
@@ -72,9 +157,71 @@ public class Repository {
     public boolean getHasWiki() {
         return hasWiki;
     }
+    public boolean getHasDownloads() {
+        return hasDownloads;
+    }
+
+    public boolean isTemplate() {
+        return isTemplate;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public boolean isAutoInit() {
+        return autoInit;
+    }
+
+    public String getGitignoreTemplate() {
+        return gitignoreTemplate;
+    }
+
+    public String getLicenseTemplate() {
+        return licenseTemplate;
+    }
+
+    public boolean isAllowSquashMerge() {
+        return allowSquashMerge;
+    }
+
+    public boolean isAllowMergeCommit() {
+        return allowMergeCommit;
+    }
+
+    public boolean isAllowMergeRebase() {
+        return allowMergeRebase;
+    }
+
+    public boolean isAllowAutoMerge() {
+        return allowAutoMerge;
+    }
+
+    public boolean isDeleteBranchOnMerge() {
+        return deleteBranchOnMerge;
+    }
+
+    public boolean isUseSquashPrTitleAsDefault() {
+        return useSquashPrTitleAsDefault;
+    }
+
+    public String getSquashMergeCommitTitle() {
+        return squashMergeCommitTitle;
+    }
+
+    public String getSquashMergeCommitMessage() {
+        return squashMergeCommitMessage;
+    }
+
+    public String getMergeCommitTitle() {
+        return mergeCommitTitle;
+    }
+
+    public String getMergeCommitMessage() {
+        return mergeCommitMessage;
+    }
 
     public JSONObject serialize() {
         return new JSONObject(JsonSerializer.toJson(this));
     }
-
 }
