edad295b9445746e247d33e81a15d7e22808f5fe
lucapitzoi <luca.pitzoi@sindria.org>
2025-03-01 02:06:04 +0100

    BaseImages: patched tomcat with reverse proxy - ingress support

--- BaseImages/tomcat/src/resources/tomcat/install.sh
+++ BaseImages/tomcat/src/resources/tomcat/install.sh
@@ -4,32 +4,36 @@ TOMCAT_VERSION=$1
 JAVA_VERSION=$2
 
 case ${TOMCAT_VERSION} in
-    8.0.53)
-        #wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.0.53/bin/apache-tomcat-8.0.53.tar.gz -P /opt
-        wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.93/bin/apache-tomcat-8.5.93.tar.gz -P /opt
+    9.0.99)
+        wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.99/bin/apache-tomcat-9.0.99.tar.gz -P /opt
         tar xzf /opt/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /opt
         rm -rf /opt/*.tar.gz
         mv /opt/apache-tomcat-${TOMCAT_VERSION} /opt/tomcat
         ;;
-    8.5.93)
-        wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.93/bin/apache-tomcat-8.5.93.tar.gz -P /opt
+    9.0.100)
+        wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.100/bin/apache-tomcat-9.0.100.tar.gz -P /opt
         tar xzf /opt/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /opt
         rm -rf /opt/*.tar.gz
         mv /opt/apache-tomcat-${TOMCAT_VERSION} /opt/tomcat
         ;;
-    9.0.80)
-        wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.tar.gz -P /opt
-        tar xzf /opt/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /opt
-        rm -rf /opt/*.tar.gz
-        mv /opt/apache-tomcat-${TOMCAT_VERSION} /opt/tomcat
-        ;;
-    10.1.13)
+    10.1.36)
         if [[ "${JAVA_VERSION}" == "8" ]]; then
             echo "Unsupported version, abort."
             exit 0
         fi
 
-        wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.13/bin/apache-tomcat-10.1.13.tar.gz -P /opt
+        wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.36/bin/apache-tomcat-10.1.36.tar.gz -P /opt
+        tar xzf /opt/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /opt
+        rm -rf /opt/*.tar.gz
+        mv /opt/apache-tomcat-${TOMCAT_VERSION} /opt/tomcat
+        ;;
+    11.0.4)
+        if [[ "${JAVA_VERSION}" == "8" ]]; then
+            echo "Unsupported version, abort."
+            exit 0
+        fi
+
+        wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.4/bin/apache-tomcat-11.0.4.tar.gz -P /opt
         tar xzf /opt/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /opt
         rm -rf /opt/*.tar.gz
         mv /opt/apache-tomcat-${TOMCAT_VERSION} /opt/tomcat
