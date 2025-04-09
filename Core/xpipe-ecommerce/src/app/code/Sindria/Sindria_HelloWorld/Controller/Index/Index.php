8a3d90b99cd252c6fc4c04ac3eea2efad09712be
dorjecurreli <dor.curreli@gmail.com>
2025-04-09 16:41:53 +0200

    fix block in chat module

---
+++ Core/xpipe-ecommerce/src/app/code/Sindria/Sindria_HelloWorld/Controller/Index/Index.php
@@ -0,0 +1,24 @@
+<?php
+namespace Sindria\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+class HelloWorld implements HttpGetActionInterface
+{
+//    protected $_pageFactory;
+//
+//    public function __construct(
+//        \Magento\Framework\App\Action\Context $context,
+//        \Magento\Framework\View\Result\PageFactory $pageFactory)
+//    {
+//        $this->_pageFactory = $pageFactory;
+//        return parent::__construct($context);
+//    }
+
+    public function execute()
+    {
+        die('pippo');
+        echo "Hello World";
+        exit;
+    }
+}
