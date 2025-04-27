d4eaaa469058b2d47b9e6bc77618686c6beba541
lucapitzoi <luca.pitzoi@sindria.org>
2025-04-28 01:25:35 +0200

    BaseImages: patched qemu

--- BaseImages/qemu/src/resources/bin/entrypoint.sh
+++ BaseImages/qemu/src/resources/bin/entrypoint.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 set -e
 
-echo "[XPIPE-KVM] Starting xpipe-kvm entrypoint..."
-echo "[XPIPE-KVM] Checking for net1 interface (macvlan from Multus)..."
+echo "[XPIPE-KVM] Entry point started..."
 
-# Aspettiamo net1 (creato da Multus CNI)
+# Wait for Multus to create net1
 while [ ! -d /sys/class/net/net1 ]; do
-  echo "[XPIPE-KVM] Waiting for net1..."
+  echo "[XPIPE-KVM] Waiting for net1 interface..."
   sleep 1
 done
 
-# Porta su l'interfaccia net1
 ip link set net1 up
 
-# Settiamo le variabili per indicare a run-qemu.sh di usare il bridge/mode corretto
-#export NETWORK_TYPE="bridge"
-#export NETWORK_IFACE="net1"
+export NETWORK_TYPE="bridge"
+export NETWORK_IFACE="net1"
 
-echo "[XPIPE-KVM] Launching standard qemux start script with NETWORK_TYPE=${NETWORK_TYPE} on ${NETWORK_IFACE}"
+echo "[XPIPE-KVM] Launching QEMU through original logic..."
 
-# Avvia lo entry.sh originale che gestisce tutto
-#exec /usr/local/bin/start.sh
-/usr/bin/tini -s /run/entry.sh
+exec /run/entry.sh
