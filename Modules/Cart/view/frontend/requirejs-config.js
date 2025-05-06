6178809545eab9858fd6f13153279c6b01cff841
lucapitzoi <luca.pitzoi@sindria.org>
2025-05-06 15:32:31 +0200

    Modules: added FontAwesome, LanguageSelector, StoreViewDefault and StoreViewItalian module - added storefront bugfix part

--- Modules/Cart/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-// var config = {
-//     map: {
-//         '*': {
-//             sindriacart:     'Sindria_Cart/js/cart'
-//         }
-//     }
-// };
---
+++ Modules/FontAwesome/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Sindria_FontAwesome',
+    __DIR__
+);
