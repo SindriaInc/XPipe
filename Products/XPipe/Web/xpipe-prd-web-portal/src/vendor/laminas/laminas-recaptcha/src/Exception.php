ddef1f82d7d9b657c236ded7bb3732d87b0c1e4c
lucapitzoi <luca.pitzoi@sindria.org>
2025-05-13 00:50:23 +0200

    update

---
+++ Products/XPipe/Web/xpipe-prd-web-portal/src/vendor/laminas/laminas-recaptcha/src/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-recaptcha for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-recaptcha/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-recaptcha/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Laminas\ReCaptcha;
+
+class Exception extends \Exception
+{
+}
