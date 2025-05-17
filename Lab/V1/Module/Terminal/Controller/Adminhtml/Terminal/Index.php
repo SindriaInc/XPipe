8b2b0cac3fad1df4b324a5644ae6009d6c58c5d4
lucapitzoi <luca.pitzoi@sindria.org>
2025-05-17 17:46:46 +0200

    Lab: init Terminal module

---
+++ Lab/V1/Module/Terminal/Controller/Adminhtml/Terminal/Index.php
@@ -0,0 +1,21 @@
+<?php
+namespace Lab\Terminal\Controller\Adminhtml\Terminal;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(Action\Context $context, PageFactory $resultPageFactory)
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
---
+++ Lab/V1/Module/Terminal/readme.md
@@ -0,0 +1,8 @@
+# Module Terminal
+
+Magento 2 module for terminal feature.
+
+## Setup module
+
+- Require your custom module: `composer require lab/module-terminal:@dev` OR `composer require lab/module-terminal:1.0.0`
+- Run setup upgrade: `php bin/magento setup:upgrade`
