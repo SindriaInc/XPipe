fd063fa4ad60fd035cd6e1249567f38aa34f1f53
lucapitzoi <luca.pitzoi@sindria.org>
2025-05-21 00:48:09 +0200

    Habemus Papam - Leone XIV

--- Core/V1/Module/Notifications/Controller/Api/Receive.php
+++ Core/V1/Module/Notifications/Controller/Api/Receive.php
@@ -1,9 +1,10 @@
 <?php
 namespace Core\Notifications\Controller\Api;
 
+use Core\Notifications\Api\Data\StatusResponseInterface;
+use Core\Notifications\Model\StatusResponse;
 use Core\Notifications\Service\NotificationService;
 use Core\Notifications\Helper\SystemEnvHelper;
-use Core\Notifications\Helper\ApiResponseHelper;
 use Core\Logger\Facade\LoggerFacade;
 use Magento\Framework\App\RequestInterface;
 
@@ -21,30 +22,30 @@ class Receive
     }
 
     /**
-     * @return array
+     * @return StatusResponseInterface
      */
-    public function execute(): array
+    public function execute(): StatusResponseInterface
     {
         try {
             $token = SystemEnvHelper::get('NOTIFICATIONS_TOKEN', '1234');
             $payload = json_decode($this->request->getContent(), true);
 
             if (!is_array($payload)) {
-                return ApiResponseHelper::sendError(400, 'Invalid or malformed JSON payload');
+                return new StatusResponse(400, false, 'Invalid or malformed JSON payload');
             }
 
             if ($token !== $this->request->getParam('token')) {
                 LoggerFacade::error('Invalid Token');
-                return ApiResponseHelper::sendError(403, 'Invalid Token');
+                return new StatusResponse(403, false, 'Invalid Token');
             }
 
             $this->notificationService->addNotification($payload);
 
-            return ApiResponseHelper::sendSuccess(200, 'Notification received');
+            return new StatusResponse(200, true, 'Notification received');
 
         } catch (\Exception $e) {
             LoggerFacade::error('Internal error', ['error' => $e]);
-            return ApiResponseHelper::sendError(500, 'Internal server error');
+            return new StatusResponse(500, false, 'Internal server error');
         }
     }
 }
