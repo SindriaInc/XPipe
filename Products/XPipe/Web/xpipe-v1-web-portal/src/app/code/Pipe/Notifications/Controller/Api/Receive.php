b58471f0ac36828b1cfa046ec1e9456db303dc1d
dorjecurreli <dor.curreli@gmail.com>
2025-05-20 15:25:35 +0200

    task-71: add base notification logic with ingress endpoint

---
+++ Products/XPipe/Web/xpipe-v1-web-portal/src/app/code/Pipe/Notifications/Controller/Api/Receive.php
@@ -0,0 +1,48 @@
+<?php
+namespace Pipe\Notifications\Controller\Api;
+
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\App\RequestInterface;
+use Pipe\Notifications\Helper\Data;
+
+class Receive
+{
+    protected $resultJsonFactory;
+    protected $helper;
+    protected $request;
+
+    public function __construct(
+        JsonFactory $resultJsonFactory,
+        Data $helper,
+        RequestInterface $request
+    ) {
+        $this->resultJsonFactory = $resultJsonFactory;
+        $this->helper = $helper;
+        $this->request = $request;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/exampleadminnewpage_helloworld_index.xml
+     *
+     * @return \Magento\Framework\Controller\Result\Json
+     */
+    public function execute(): \Magento\Framework\Controller\Result\Json
+    {
+
+        $result = $this->resultJsonFactory->create();
+        try {
+            $payload = json_decode($this->request->getContent(), true);
+
+            $title = $payload['title'] ?? 'Untitled';
+            $message = $payload['message'] ?? '';
+            $severity = $payload['severity'] ?? 'notice';
+            $source = $payload['source'] ?? '';
+
+            $this->helper->addNotification($title, $message, $severity, '', $source);
+
+            return $result->setData(['success' => true]);
+        } catch (\Exception $e) {
+            return $result->setData(['success' => false, 'message' => 'Internal error']);
+        }
+    }
+}
