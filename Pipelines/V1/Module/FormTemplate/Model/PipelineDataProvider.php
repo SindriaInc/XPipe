61a24c660b440cf6289192aa5046688d47510302
lucapitzoi <luca.pitzoi@sindria.org>
2025-05-21 18:56:04 +0200

    test

--- Pipelines/V1/Module/FormTemplate/Model/PipelineDataProvider.php
+++ Pipelines/V1/Module/FormTemplate/Model/PipelineDataProvider.php
@@ -1,29 +1,36 @@
 <?php
 namespace Pipelines\FormTemplate\Model;
 
+use Magento\Framework\Data\Collection;
+use Magento\Framework\ObjectManagerInterface;
 use Magento\Ui\DataProvider\AbstractDataProvider;
 
 class PipelineDataProvider extends AbstractDataProvider
 {
-    protected $loadedData;
+    protected $collection;
+
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        ObjectManagerInterface $objectManager,
+        array $meta = [],
+        array $data = []
+    ) {
+        // Creiamo una collection fake
+        $this->collection = $objectManager->create(Collection::class);
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+    }
 
     public function getData()
     {
-        if (isset($this->loadedData)) {
-            return $this->loadedData;
-        }
-
-        $items = [
-            ['pipeline_id' => 1, 'name' => 'CI Pipeline', 'author' => 'Alice', 'created_at' => '2024-10-01'],
-            ['pipeline_id' => 2, 'name' => 'CD Pipeline', 'author' => 'Bob', 'created_at' => '2024-10-15'],
-            ['pipeline_id' => 3, 'name' => 'DevOps Flow', 'author' => 'Charlie', 'created_at' => '2025-01-20'],
+        return [
+            'items' => [
+                ['pipeline_id' => 1, 'name' => 'CI Pipeline', 'author' => 'Alice', 'created_at' => '2024-10-01'],
+                ['pipeline_id' => 2, 'name' => 'CD Pipeline', 'author' => 'Bob', 'created_at' => '2024-10-15'],
+                ['pipeline_id' => 3, 'name' => 'DevOps Flow', 'author' => 'Charlie', 'created_at' => '2025-01-20'],
+            ],
+            'totalRecords' => 3
         ];
-
-        $this->loadedData = [
-            'totalRecords' => count($items),
-            'items' => $items
-        ];
-
-        return $this->loadedData;
     }
 }
