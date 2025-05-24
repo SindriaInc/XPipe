20ddeb5a0155bba46e4755de0f0532d9ff107b15
dorjecurreli <dor.curreli@gmail.com>
2025-05-24 13:57:49 +0200

    task-72: add collection

--- Pipelines/V1/Module/PipeManager/Ui/Component/Listing/GithubActionsDataProvider.php
+++ Pipelines/V1/Module/PipeManager/Ui/Component/Listing/GithubActionsDataProvider.php
@@ -1,10 +1,12 @@
 <?php
 namespace Pipelines\PipeManager\Ui\Component\Listing;
 
+use Core\Logger\Facade\LoggerFacade;
 use Magento\Framework\Data\Collection as DataCollection;
 use Magento\Framework\Data\Collection\EntityFactoryInterface;
 use Magento\Framework\DataObject;
 use Magento\Ui\DataProvider\AbstractDataProvider;
+use Pipelines\PipeManager\Model\Listing\GitHubActionsCollection;
 use Pipelines\PipeManager\Service\GithubActionsService;
 
 class GithubActionsDataProvider extends AbstractDataProvider
@@ -21,11 +23,20 @@ class GithubActionsDataProvider extends AbstractDataProvider
         array $meta = [],
         array $data = []
     ) {
+        LoggerFacade::debug('GithubActionsDataProvider::__construct', [
+            'name' => $name,
+            'primaryFieldName' => $primaryFieldName,
+            'requestFieldName' => $requestFieldName
+        ]);
+
+
+
         $this->githubActionsService = $githubActionsService;
 
         // Recupera dati GitHub (o mocka in caso di errore)
         $runs = $this->githubActionsService->getLatestRuns();
-        $this->collection = new DataCollection($entityFactory);
+
+        $this->collection = new GitHubActionsCollection($entityFactory, $runs);
 
         foreach ($runs as $run) {
             $this->collection->addItem(new DataObject([
