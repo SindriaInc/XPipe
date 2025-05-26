9842545d4d45c75367d1034614b2b68a8ea14aba
lucapitzoi <luca.pitzoi@sindria.org>
2025-05-27 00:30:26 +0200

    Pipelines: added back button on show logs - PipeManager module

--- Pipelines/V1/Module/PipeManager/Block/Adminhtml/Pipeline/Run/Navbar.php
+++ Pipelines/V1/Module/PipeManager/Block/Adminhtml/Pipeline/Run/Navbar.php
@@ -1,41 +1,66 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
+ * Copyright © Sindria, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Pipelines\PipeManager\Block\Adminhtml\Pipeline\Run;
+
+use Magento\Backend\Block\Widget\Container;
+
 /**
  * @api
  * @since 100.0.2
  */
-class Navbar extends \Magento\Backend\Block\Template
+class Navbar extends Container
 {
+    //protected $_headerText = 'Run Logs';
+
     /**
-     * Block constructor
+     * Navbar mage constructor
      *
      * @return void
      */
-//    protected function _construct()
-//    {
-//
-//        $this->_blockGroup = 'Pipelines_PipeManager';
-//        $this->_controller = 'pipeline_run/show';
-//
-//        //        $this->_headerText = __('Custom Variables');
-//        parent::_construct();
-//        $this->buttonList->update('add', 'label', __('Add New Variable'));
-////        dd($this);
-//    }
+    protected function _construct()
+    {
+        parent::_construct();
 
+        $this->buttonList->add(
+            'back',
+            [
+                'label'   => __('Back'),
+                'class'   => 'back',
+                'onclick' => sprintf(
+                    "setLocation('%s');",
+                    $this->getBackUrl()
+                )
+            ],
+            -1 // priorità (prima di altri)
+        );
+    }
+
+    /**
+     * URL di destinazione quando si clicca “Back”.
+     * Puoi leggere ?back=... dalla request o
+     * puntare a una route statica.
+     */
     public function getBackUrl(): string
     {
-//        $back = $this->getRequest()->getParam('back');
-//
-//        if ($back) {
-//            return $back;
-//        }
+        // 1. Priorità assoluta: parametro ?back= ...
+        if ($target = $this->getRequest()->getParam('back')) {
+            return $target;
+        }
 
-        // Fallback
-        return $this->getUrl('pipemanager/pipeline/index');
+        // 2. In assenza di ?back=..., usa il Referer inviato dal browser
+        if ($referer = (string) $this->getRequest()->getServer('HTTP_REFERER')) {
+            // --- facoltativo: metti una guardia di sicurezza ---
+            // consenti solo URL interni alla tua installazione backend
+            $baseUrl = $this->getBaseUrl();               // dal Widget\Container
+            if (strpos($referer, $baseUrl) === 0) {
+                return $referer;           // include tutti i parametri
+            }
+        }
+
+        // 3. Fallback statico (safe)
+        return $this->getUrl('pipemanager/index');
     }
 }
