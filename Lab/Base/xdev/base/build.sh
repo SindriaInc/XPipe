6fe4bc42bd83c1c62b826a977d775444df21902c
lucapitzoi <luca.pitzoi@sindria.org>
2025-06-01 10:42:57 +0200

    Lab: fix upgrade to fedora 42

--- Lab/Base/xdev/base/build.sh
+++ Lab/Base/xdev/base/build.sh
@@ -16,25 +16,31 @@ if [[ -z "$3" ]]; then
 fi
 
 if [[ -z "$4" ]]; then
-    echo "Provide a kubectl arch as fourth argument (eg. amd64, arm64)"
+    echo "Provide a platform arch as fourth argument (eg. linux/amd64, linux/arm64/v8)"
     exit 1
 fi
 
 if [[ -z "$5" ]]; then
-    echo "Provide a awscli arch as fifth argument (eg. x86_64, aarch64)"
+    echo "Provide a kubectl arch as fifth argument (eg. amd64, arm64)"
+    exit 1
+fi
+
+if [[ -z "$6" ]]; then
+    echo "Provide a awscli arch as sixth argument (eg. x86_64, aarch64)"
     exit 1
 fi
 
 IMAGE_NAME=$1
 TAG_VERSION=$2
 TAG_ARCH=$3
-KUBECTL_ARCH=$4
-AWSCLI_ARCH=$5
+PLATFORM=$4
+KUBECTL_ARCH=$5
+AWSCLI_ARCH=$6
 
 HOST_USER_UID=1000
 TIMEZONE=Europe/Rome
 HOST_DOCKER_GROUP_UID=975
-FEDORA_VERSION=43
+FEDORA_VERSION=42
 XDEV_SINDRIA_USER_PASSWORD=sindria
 
 
@@ -44,6 +50,7 @@ if [ "${TAG_VERSION}" == "test" ]; then
     --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_ARCH} \
     --tag ${IMAGE_NAME}:latest \
     --build-arg ARCH=${TAG_ARCH} \
+    --build-arg PLATFORM=${PLATFORM} \
     --build-arg FEDORA_VERSION=${FEDORA_VERSION} \
     --build-arg TAG_VERSION=${TAG_VERSION} \
     --build-arg HOST_USER_UID=${HOST_USER_UID} \
@@ -59,6 +66,7 @@ if [ "${TAG_VERSION}" != "test" ]; then
     --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_ARCH} \
     --tag ${IMAGE_NAME}:latest \
     --build-arg ARCH=${TAG_ARCH} \
+    --build-arg PLATFORM=${PLATFORM} \
     --build-arg FEDORA_VERSION=${FEDORA_VERSION} \
     --build-arg TAG_VERSION=${TAG_VERSION} \
     --build-arg HOST_USER_UID=${HOST_USER_UID} \
