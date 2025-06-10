12e4f411a477a10d8406aeb1f9ce6d122e1c6101
lucapitzoi <luca.pitzoi@sindria.org>
2025-06-10 10:37:02 +0200

    fix

--- Products/XPipe/V1/xpipe-v1-web-portal/bin/automatic_installer.sh
+++ Products/XPipe/V1/xpipe-v1-web-portal/bin/automatic_installer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Admin configuration
 ADMIN_FIRST_NAME=Carbon
 ADMIN_LAST_NAME=User
@@ -10,9 +12,11 @@ ADMIN_PASSWORD=admin123
 # App configuration
 APP_NAME=xpipe-v1-web-portal
 
+COMMAND_01="composer install --working-dir /var/www/app"
+docker exec -t ${APP_NAME} ${COMMAND_01}
+
 bash bin/magento_setup.sh ${ADMIN_FIRST_NAME} ${ADMIN_LAST_NAME} ${ADMIN_EMAIL} ${ADMIN_USERNAME} ${ADMIN_PASSWORD}
 
-COMMAND_01="composer install"
 COMMAND_02="php bin/magento setup:upgrade"
 COMMAND_03="php bin/magento setup:di:compile"
 COMMAND_04="php bin/magento setup:static-content:deploy it_IT en_US -f"
@@ -20,7 +24,6 @@ COMMAND_05="php bin/magento indexer:reindex"
 COMMAND_06="php bin/magento deploy:mode:set developer"
 COMMAND_07="php bin/magento cache:flush"
 
-docker exec -t ${APP_NAME} ${COMMAND_01}
 docker exec -t ${APP_NAME} ${COMMAND_02}
 docker exec -t ${APP_NAME} ${COMMAND_03}
 docker exec -t ${APP_NAME} ${COMMAND_04}
