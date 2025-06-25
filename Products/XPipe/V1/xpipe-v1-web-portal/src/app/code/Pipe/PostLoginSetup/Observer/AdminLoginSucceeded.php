36197ad24cad23f23ef5d8d5fefc232df27be6fd
dorcurreli <dor.curreli@gmail.com>
2025-06-25 10:55:06 +0200

    task-101: init post login setup module

---
+++ Products/XPipe/V1/xpipe-v1-web-portal/src/app/code/Pipe/PostLoginSetup/Observer/AdminLoginSucceeded.php
@@ -0,0 +1,21 @@
+<?php
+namespace Pipe\PostLoginSetup\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+
+
+class AdminLoginSucceeded implements ObserverInterface
+{
+
+
+    public function __construct()
+    {
+
+    }
+
+    public function execute(Observer $observer)
+    {
+
+    }
+}
---
+++ Products/XPipe/V1/xpipe-v1-web-portal/src/app/code/Pipe/PostLoginSetup/readme.md
@@ -0,0 +1,8 @@
+# Module Logo
+
+Magento 2 module for after login setup.
+
+## Setup module
+
+- Require your custom module: `composer require pipe/module-post-login-setup:@dev` OR `composer require pipe/module-post-login-setup:1.0.0`
+- Run setup upgrade: `php bin/magento setup:upgrade`
