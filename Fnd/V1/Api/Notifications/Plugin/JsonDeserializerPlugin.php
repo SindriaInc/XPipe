841fdeb30d0a78a11a14a71ffa6fd137dfdd0546
dorcurreli <dor.curreli@gmail.com>
2025-06-27 12:40:46 +0200

    task-6: cleanup

--- Fnd/V1/Api/Notifications/Plugin/JsonDeserializerPlugin.php
+++ Fnd/V1/Api/Notifications/Plugin/JsonDeserializerPlugin.php
@@ -4,18 +4,10 @@ namespace Fnd\Notifications\Plugin;
 use Core\Logger\Facade\LoggerFacade;
 use Core\MicroFramework\Model\StatusResponse;
 use Fnd\Notifications\Helper\NotificationsHelper;
-use Magento\Framework\Webapi\Exception as WebapiException;
-use Magento\Framework\Phrase;
+
 
 class JsonDeserializerPlugin
 {
-//    protected $myJsonValidator;
-//
-//    public function __construct(\Vendor\Module\Helper\JsonValidator $myJsonValidator)
-//    {
-//        $this->myJsonValidator = $myJsonValidator;
-//    }
-
     public function aroundDeserialize(
         \Magento\Framework\Webapi\Rest\Request\Deserializer\Json $subject,
         callable $proceed,
