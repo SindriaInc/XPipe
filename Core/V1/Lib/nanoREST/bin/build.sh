c8bfece07bd635859aa58559003abff260bda69e
lucapitzoi <luca.pitzoi@sindria.org>
2025-06-30 13:42:30 +0200

    cleanup

--- Core/V1/Lib/nanoREST/bin/build.sh
+++ Core/V1/Lib/nanoREST/bin/build.sh
@@ -10,7 +10,7 @@ NC='\033[0m' #No Color
 NOW=$(date "+%Y-%m-%d_%H-%M-%S")
 
 CONTAINER_NAME=nanoREST
-NANOREST_VERSION=2.4.0
+NANOREST_VERSION=2.4.1
 
 echo -e "${BLUE}Building and Packaging...${NC}"
 docker exec -t ${CONTAINER_NAME} bash -c "mvn compile; mvn package"
