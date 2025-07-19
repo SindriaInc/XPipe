794cd0fa745a48e63cc7b5e92c08f019c086fc73
lucapitzoi <luca.pitzoi@sindria.org>
2025-07-19 17:00:50 +0200

    update

--- IaC/wopr/update_dockerhub.sh
+++ IaC/wopr/update_dockerhub.sh
@@ -3,6 +3,19 @@
 # export AWS_PROFILE=default
 #AWS_PROFILE=default
 
+# Delete existing docker hub secret on all namespaces
+kubectl delete secret sindria-dockerhub -n bastion
+kubectl delete secret sindria-dockerhub -n cdn
+kubectl delete secret sindria-dockerhub -n monitoring
+kubectl delete secret sindria-dockerhub -n sindria-mc
+kubectl delete secret sindria-dockerhub -n sindria-sec
+kubectl delete secret sindria-dockerhub -n xpipe-cloud
+kubectl delete secret sindria-dockerhub -n xpipe-demo
+kubectl delete secret sindria-dockerhub -n xpipe-dev
+kubectl delete secret sindria-dockerhub -n xpipe-sindria
+kubectl delete secret sindria-dockerhub -n xpipe-dorjecurreli
+
+
 # Update docker hub secret on all namespaces
 kubectl apply -f sindria-dockerhub.yaml -n bastion
 kubectl apply -f sindria-dockerhub.yaml -n cdn
@@ -13,3 +26,4 @@ kubectl apply -f sindria-dockerhub.yaml -n xpipe-cloud
 kubectl apply -f sindria-dockerhub.yaml -n xpipe-demo
 kubectl apply -f sindria-dockerhub.yaml -n xpipe-dev
 kubectl apply -f sindria-dockerhub.yaml -n xpipe-sindria
+kubectl apply -f sindria-dockerhub.yaml -n xpipe-dorjecurreli
