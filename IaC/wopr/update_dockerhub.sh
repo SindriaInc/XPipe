dfed37a31acf2db305bb96c6cdd46f59eb8b171a
lucapitzoi <luca.pitzoi@sindria.org>
2025-08-19 01:40:12 +0200

    IaC

--- IaC/wopr/update_dockerhub.sh
+++ IaC/wopr/update_dockerhub.sh
@@ -5,13 +5,14 @@
 
 # Delete existing docker hub secret on all namespaces
 kubectl delete secret sindria-dockerhub -n bastion
-kubectl delete secret sindria-dockerhub -n besteam-iaas
+#kubectl delete secret sindria-dockerhub -n besteam-dev
 #kubectl delete secret sindria-dockerhub -n cdn
 kubectl delete secret sindria-dockerhub -n monitoring
 kubectl delete secret sindria-dockerhub -n mh-website
 kubectl delete secret sindria-dockerhub -n lmd-website
 kubectl delete secret sindria-dockerhub -n sindria-mc
 #kubectl delete secret sindria-dockerhub -n sindria-sec
+kubectl delete secret sindria-dockerhub -n xpipe-besteam
 kubectl delete secret sindria-dockerhub -n xpipe-cloud
 #kubectl delete secret sindria-dockerhub -n xpipe-demo
 kubectl delete secret sindria-dockerhub -n xpipe-dev
@@ -22,13 +23,14 @@ kubectl delete secret sindria-dockerhub -n xpipe-dorjecurreli
 
 # Update docker hub secret on all namespaces
 kubectl apply -f sindria-dockerhub.yaml -n bastion
-kubectl apply -f sindria-dockerhub.yaml -n besteam-iaas
+#kubectl apply -f sindria-dockerhub.yaml -n besteam-dev
 #kubectl apply -f sindria-dockerhub.yaml -n cdn
 kubectl apply -f sindria-dockerhub.yaml -n monitoring
 kubectl apply -f sindria-dockerhub.yaml -n mh-website
 kubectl apply -f sindria-dockerhub.yaml -n lmd-website
 kubectl apply -f sindria-dockerhub.yaml -n sindria-mc
 #kubectl apply -f sindria-dockerhub.yaml -n sindria-sec
+kubectl apply -f sindria-dockerhub.yaml -n xpipe-besteam
 kubectl apply -f sindria-dockerhub.yaml -n xpipe-cloud
 #kubectl apply -f sindria-dockerhub.yaml -n xpipe-demo
 kubectl apply -f sindria-dockerhub.yaml -n xpipe-dev
