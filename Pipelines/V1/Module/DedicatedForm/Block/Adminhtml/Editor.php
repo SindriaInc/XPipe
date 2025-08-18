466f9eb4529c499b30f54335e1583ec7fac71ccf
dorcurreli <dor.curreli@gmail.com>
2025-08-18 15:20:29 +0200

    setup dedicated form fields

---
+++ Pipelines/V1/Module/DedicatedForm/Block/Adminhtml/Editor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pipelines\DedicatedForm\Block\Adminhtml;
+class Editor extends \Magento\Backend\Block\Template
+{
+    /**
+     * Block template.
+     *
+     * @var string
+     */
+    protected $_template = 'editor.phtml';
+
+}
